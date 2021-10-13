7d3d522262095c143bf446fd470fe188bffcd270
HEE / 정은희 <70689381+EunHee-Jeong@users.noreply.github.com>
2021-10-13 12:30:22 +0900

    Usage example
    Read all CSV files & Writing data to a file & Loading data from the file

--- CSV_n_PICKLE.py
+++ CSV_n_PICKLE.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 
 
-# CSV File Reading and Writing
-import glob, csv 
+# MARK: Read all CSV files
+import glob, csv
 
-files = glob.glob('/python/python_tutorial/data/class_score_??.csv')    # glob()를 사용해 파일을 string으로 읽어옴
+files = glob.glob('data/class_score_??.csv')    # files에 class_score_en, class_score_kr 파일을 가져와 저장함
 all_data = []
 for file in files:
-    with open(file, 'r') as f:  # file-obj f 생성
-        csv_reader = csv.reader(f)  # CSV-reader-obj csv_reader 생성
+    with open(file, 'r') as f:  # file obj 생성
+        csv_reader = csv.reader(f)  # csv reader obj 생성
         data = []   # 하나의 파일에 해당하는 data를 모아줄 리스트
-        for line in csv_reader: # single line씩 받아와서
-            if line and not line[0].strip().startswitch('#'):   # 주석이 아니면
-                data.append([int(val) for val in line]) # 각각의 value들을 그대로 넣어줌 (list comprehension)
-            all_data += data    # merge
+        for line in csv_reader:
+            if line and not line[0].strip().startswitch('#'):   # 주석이 아니라면
+                data.append([int(val) for val in line])
+            all_data += data
+            
 
-# Writing data to a file
-import pickle
-with open('class_score_all.pickle', 'wb') as f:
-    pickle.dump((files, all_data), f)  # 저장할 변수가 두개이므로 tuple로 넣음
-    
-# Loading data from the file
+# MARK: Pickle
+"""
+파이썬의 data들을 file로 내보내거나 가져오고 싶을 때,
+변수들을 그대로 저장해 사용할 수 있는 클래스임. (= 덩어리로 쓸 수 있음)
+즉 Selialization 하는 것~
+그렇기 때문에 파이썬 객체를 파일 형식에 관계 없이 save & load 할 수 있다.
+"""
+# MARK: Writing data to a file 
+import pickle 
+with open('class_score_all.pickle', 'wb') as f: 
+    pickle.dump((files,all_data), f)    # 저장할 변수를 tuple로 넣음 
+# MARK: Loading data from the file 
 with open('class_score_all.pickle', 'rb') as f:
-    _, data = pickle.load(f)    # 두번째 것만 꺼냄
+    _, data = pickle.load(f)    # 두 번째 것만 꺼내옴 
     print(data)
