6eac37632f1ee48d33d6fde2ee45dccfb5978bc6
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-12-06 13:43:32 +0900

    🚚[MOVE] 데이터 파일 경로 수정 (#1)

--- Term-Project/oss-term-project.py
+++ Term-Project/oss-term-project.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 1-1. 데이터 불러오기
-bus_data = pd.read_csv("./data/BUS_STATION_BOARDING_MONTH_202110.csv", encoding = 'cp949')
+bus_data = pd.read_csv("./drive/MyDrive/data/BUS_STATION_BOARDING_MONTH_202110.csv", encoding = 'cp949')
 bus_data.head()
 bus_data.info()
 
@@ -13,3 +13,7 @@ sorted(list(set(bus_data['노선명'])))
 sorted(list(set(bus_data['역명'])))
 sorted(list(set(bus_data['승차총승객수'])))
 sorted(list(set(bus_data['하차총승객수'])))
+
+# 2-1. 불필요한 칼럼 제거
+my_bus_data = bus_data.drop(columns={'버스정류장ARS번호', '등록일자'})
+my_bus_data
