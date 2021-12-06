bd8b299893edbdca4b3649acf22b92b9aa322339
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-12-06 14:31:44 +0900

    ✨[FEAT] 202번 버스의 평균 승하차 인원 추출하기 완료 (#1)

--- Term-Project/oss-term-project.py
+++ Term-Project/oss-term-project.py
@@ -17,3 +17,21 @@ sorted(list(set(bus_data['하차총승객수'])))
 # 2-1. 불필요한 칼럼 제거
 my_bus_data = bus_data.drop(columns={'버스정류장ARS번호', '등록일자'})
 my_bus_data
+
+# 2-2. 202번 버스의 승차 인원 컬럼만 추출
+num = '202'
+bus_st_202 = bus_st[bus_st['노선번호']==num]
+bus_get_on = pd.DataFrame()
+bus_get_on['노선번호'] = bus_st_202['노선번호']
+for i in range(int((len(my_bus_data.columns)-3)/2)):
+    bus_get_on[bus_st_202.columns[3+2*i]] = bus_st_202[bus_st_202.columns[3+2*i]]
+bus_get_on
+
+# 2-3. 202번 버스의 하차 인원 컬럼만 추출
+num = '202'
+bus_st_202 = bus_st[bus_st['노선번호']==num]
+bus_get_off = pd.DataFrame()
+bus_get_off['노선번호'] = bus_st_202['노선번호']
+for i in range(int((len(my_bus_data.columns)-3)/2)):
+    bus_get_off[bus_st_202.columns[4+2*i]] = bus_st_202[bus_st_202.columns[4+2*i]]
+bus_get_off
